e188dfe688ab18b32cb30990241c2eada2ea67d9
Vithurshan PAKIRATHAN <vithurshanpakirathan@hotmail.fr>
2021-04-08 11:45:45 +0200

    affichageSearch

--- GUI/src/gui/FXMLDocumentController.java
+++ GUI/src/gui/FXMLDocumentController.java
@@ -11,25 +11,44 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author vithu
  */
 public class FXMLDocumentController implements Initializable {
-    
     @FXML
-    private Label label;
+    private Button button;
+    @FXML
+    private Label label_date;
+    @FXML
+    private Label label_hour;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        label_date.setText(getDate());
+        label_hour.setText(getHour());
     }    
     
+    public String getDate(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");  
+        LocalDateTime now = LocalDateTime.now();  
+        return dtf.format(now);  
+    }
+    public String getHour(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");  
+        LocalDateTime now = LocalDateTime.now();  
+        return dtf.format(now);  
+    }
 }
